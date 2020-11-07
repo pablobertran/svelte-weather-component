769a3ed29cf21524319f91fb84c98f3ba880e394
Pablo Bertran <info@pablobertran.com>
2020-11-07 21:54:13 +0100

    - First commit

--- src/config/settings.js
+++ src/config/settings.js
@@ -6,32 +6,3 @@ export const settings = {
     lang: 'es',
     unit: 'metric'
 }
-/*
-var unirest = require("unirest");
-
-var req = unirest("GET", "https://community-open-weather-map.p.rapidapi.com/weather");
-
-req.query({
-    "q": "London,uk",
-    "lat": "0",
-    "lon": "0",
-    "callback": "test",
-    "id": "2172797",
-    "lang": "null",
-    "units": "\"metric\" or \"imperial\"",
-    "mode": "xml, html"
-});
-
-req.headers({
-    "x-rapidapi-key": "SLZoJGKK8AmshSRaL69CadFK8JJjp1y57g3jsnh4kucKGacsE0",
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com/",
-    "useQueryString": true
-});
-
-
-req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-
-    console.log(res.body);
-});
-*/
